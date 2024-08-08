d4e44fe2276abf3bfff8675578cf999047f96b96
momchil-anachkov <momchil.anachkov@gmail.com>
2024-08-08 17:01:00 +0300

    improve readability

--- src/main.ts
+++ src/main.ts
@@ -3,34 +3,26 @@ import express, { Request } from 'express';
 import bodyParser from 'body-parser';
 import { join } from 'path';
 
-// Create and initialize EC context
-// (better do it once and reuse it)
 var ec = new EC('p521');
 
-// Generate keys
-var key = ec.genKeyPair();
+var keyPair = ec.genKeyPair();
 
-console.log('public:', key.getPublic('hex'));
-console.log('private:', key.getPrivate('hex'));
+console.log('public:', keyPair.getPublic('hex'));
+console.log('private:', keyPair.getPrivate('hex'));
 
 const server = express();
-
-key.inspect()
-
 server.use(bodyParser.json());
 
-let path = join(__dirname, 'public');
-server.use('/', express.static(path));
+let staticPath = join(__dirname, 'public');
+server.use('/', express.static(staticPath));
 
 server.post('/sign', (req: Request<{ message: string }>, res) => {
-    console.log(req.body);
-    const signature = key.sign(req.body.message).toDER('hex');
+    const signature = keyPair.sign(req.body.message).toDER('hex');
     res.json(JSON.stringify({ signature }));
 });
 
 server.post('/verify', (req: Request<{ message: string, signature: string }>, res) => {
-    const verified = key.verify(req.body.message, req.body.signature);
-    console.log('Verified:', verified);
+    const verified = keyPair.verify(req.body.message, req.body.signature);
     res.send(`Verified: ${verified}`);
 });
 
